6551d32eedc62a58c750810cf03b16c82d931d99
Leik Lima-Eriksen <leik.lima-eriksen@lyse.net>
2019-04-11 18:56:18 +0200

    Add BackgroundImage component that show covers page with image and has fallback

---
+++ src/components/BackgroundImage/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+const fallbackImagePath = '/fallback_background.jpg'
+
+class BackgroundImage extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		const { src } = this.props
+		let imagePath
+
+		if (!src) imagePath = fallbackImagePath
+		else imagePath = src
+
+		return (
+			<img
+				src={imagePath}
+				style={{
+					width: '100vw',
+					zIndex: '-1',
+					// CSS3-type vertical and horizontal centering:
+					// (source: https://www.w3.org/Style/Examples/007/center.en.html)
+					position: 'fixed',
+					top: '50%',
+					left: '50%',
+					marginRight: '-50%',
+					transform: 'translate(-50%, -50%)'
+				}}
+			/>
+		)
+	}
+}
+
+export default BackgroundImage
